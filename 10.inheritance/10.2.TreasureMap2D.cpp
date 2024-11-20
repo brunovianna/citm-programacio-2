30eb64c6f4e131893b7fe8cbfafac46e9a0ac091
bruno vianna <bruno@pobox.com>
2024-11-20 08:25:11 +0100

    Create 10.2.TreasureMap2D.cpp

---
+++ 10.inheritance/10.2.TreasureMap2D.cpp
@@ -0,0 +1,231 @@
+// 4.2-TreasureMap.cpp : Este archivo contiene la función "main". La ejecución del programa comienza y termina ahí.
+//
+
+#include <iostream>
+#include <cstdlib>
+#include <ctime>
+
+using namespace std;
+
+
+const int ARRAY_SIZE = 20;
+
+const int ARRAY_X = 20;
+const int ARRAY_Y = 20;
+
+class TreasureMapClass {
+private:
+
+    char charMap[ARRAY_SIZE]{};
+
+public:
+    int treasure;
+    int player;
+    TreasureMapClass() {
+
+        for (int i = 0; i < ARRAY_SIZE; i++) {
+
+            charMap[i] = '.';
+        }
+
+        // Place the treasure
+        treasure = std::rand() % ARRAY_SIZE;
+        charMap[treasure] = 'T';
+
+        //place the player at the leftmost position
+        player = 0;
+
+        charMap[player] = '@';
+
+    }
+    void print() {
+        for (char* i = charMap; i < charMap + ARRAY_SIZE; i++) {
+            cout << *i;
+        }
+        cout << std::endl;
+    }
+        bool move(char m) {
+        switch (m) {
+        case 'a':
+            if (player > 0) {
+                charMap[player] = '.';
+
+                player--;
+                charMap[player] = '@';
+                cout << "Moved left by 1" <<  endl;
+            }
+            else {
+                cout << "Can't move left. You're at the start of the line." <<  endl;
+            }
+            break;
+        case 'd':
+            if (player < ARRAY_SIZE-1) {
+                charMap[player] = '.';
+
+                player++;
+                charMap[player] = '@';
+                cout << "Moved right by 1" << endl;
+            }
+            else {
+                cout << "Can't right left. You're at the end of the line." << endl;
+            }
+            break;
+        case 'q':
+           cout << "Thanks for playing!" <<  endl;
+           return false;
+        }
+        return true;
+    }
+
+    bool checkTreasure() {
+        if ((player == treasure)) {
+            return true;
+        }
+        else {
+            return false;
+        }
+    }
+};
+
+
+
+class TreasureMapClass2D: public TreasureMapClass {
+private:
+
+    char charMap2D[ARRAY_X][ARRAY_Y];
+
+public:
+    int treasure_x, treasure_y;
+    int player_x, player_y;
+    TreasureMapClass2D() {
+
+        for (int j=0;j< ARRAY_Y; j++) {
+
+            for (int i = 0; i < ARRAY_X; i++) {
+
+                charMap2D[i][j] = '.';
+            }
+        }
+
+        // Place the treasure
+        treasure_x = std::rand() % ARRAY_X;
+        treasure_y = std::rand() % ARRAY_Y;
+        charMap2D[treasure_x][treasure_y] = 'T';
+
+        //place the player at the leftmost position
+        player_x = 0;
+        player_y = 0;
+        charMap2D[player_x][player_y] = '@';
+
+    }
+
+    bool move(char m) {
+        switch (m) {
+        case 'a':
+            if (player_x > 0) {
+                charMap2D[player_x][player_y] = '.';
+
+                player_x--;
+                charMap2D[player_x][player_y] = '@';
+                cout << "Moved left by 1" <<  endl;
+            }
+            else {
+                cout << "Can't move left. You're at the start of the line." <<  endl;
+            }
+            break;
+        case 'd':
+            if (player_x < ARRAY_X-1) {
+                charMap2D[player_x][player_y] = '.';
+
+                player_x++;
+                charMap2D[player_x][player_y] = '@';
+                cout << "Moved right by 1" << endl;
+            }
+            else {
+                cout << "Can't right left. You're at the end of the line." << endl;
+            }
+            break;
+        case 'w':
+            if (player_y > 0) {
+                charMap2D[player_x][player_y] = '.';
+
+                player_y--;
+                charMap2D[player_x][player_y] = '@';
+                cout << "Moved up by 1" << endl;
+            }
+            else {
+                cout << "Can't move up. You're at the top of the board." << endl;
+            }
+            break;
+        case 's':
+            if (player_y < ARRAY_Y-1) {
+                charMap2D[player_x][player_y] = '.';
+
+                player_y++;
+                charMap2D[player_x][player_y] = '@';
+                cout << "Moved down by 1" << endl;
+            }
+            else {
+                cout << "Can't move down. You're at the bottom of the board." << endl;
+            }
+            break;
+        case 'q':
+           cout << "Thanks for playing!" <<  endl;
+           return false;
+        }
+        return true;
+    }
+
+    void print() {
+        for (int j = 0; j < ARRAY_Y; j++) {
+
+            for (int i = 0; i < ARRAY_X; i++) {
+
+                cout << charMap2D[i][j];
+            }
+            cout << endl;
+        }
+    }
+
+
+    bool checkTreasure() {
+        if ((player_x == treasure_x) && (player_y == treasure_y)) {
+            return true;
+        }
+        else {
+            return false;
+        }
+    }
+};
+
+
+int main() {
+    srand(std::time(nullptr));
+
+    TreasureMapClass2D treasureMap;
+
+
+    cout << "Find the treasure (T)." << endl;
+    cout << "Commands: 'a','s','d','w' to move  'q' to quit." << endl << endl;
+
+    while (true) {
+        treasureMap.print();
+
+        if (treasureMap.checkTreasure()) {
+            std::cout << "Congratulations! You found the treasure!" << endl;
+            break;
+        }
+
+        char move;
+        cout << "Enter your move: ";
+        cin >> move;
+
+        if (!treasureMap.move(move)) {
+            return (0);
+        }
+
+
+    }
+
+    return 0;
+}
