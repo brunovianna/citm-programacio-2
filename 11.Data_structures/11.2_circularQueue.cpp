6b023125997eed4e11cb2d2abf2d1d63e11321ab
bruno vianna <bruno@pobox.com>
2024-12-05 13:01:18 +0100

    Create 11.2_circularQueue.cpp

---
+++ 11.Data_structures/11.2_circularQueue.cpp
@@ -0,0 +1,135 @@
+// circularQueue.cpp : Este archivo contiene la función "main". La ejecución del programa comienza y termina ahí.
+//
+
+#include <iostream>
+
+using namespace std;
+
+class CircularQueue {
+public:
+    CircularQueue(int capacity = 10) {
+        _capacity = capacity;
+        _array = new int[_capacity];
+        _front = -1;
+        _back = -1;
+        _size = 0;
+    }
+
+    ~CircularQueue() {
+        delete[] _array;
+    }
+
+    void enqueue(int value) {
+        if (_size == _capacity) {
+            resize(); 
+        }
+
+        if (_front == -1) {
+            _front = 0;
+        }
+
+        _back++;
+        if (_back == _capacity) { 
+            _back = 0;
+        }
+
+        _array[_back] = value;
+        ++_size;
+    }
+
+    int dequeue() {
+        if (empty()) {
+            cout <<"Queue is empty. Cannot dequeue.\n";
+        }
+
+        int frontValue = _array[_front];
+        _front++;
+        if (_front == _capacity) { 
+            _front = 0;
+        }
+
+        --_size;
+
+        if (_size == 0) { 
+            _front = -1;
+            _back = -1;
+        }
+
+        return frontValue;
+    }
+
+ 
+    int size() const {
+        return _size;
+    }
+
+    bool empty() const {
+        return _size == 0;
+    }
+
+    void print() const {
+        if (empty()) {
+            cout << "Queue is empty. Nothing to print.\n";
+            return;
+        }
+
+        std::cout << "Queue elements: ";
+        int index = _front;
+        for (int i = 0; i < _size; ++i) {
+            std::cout << _array[index] << " ";
+            index++;
+            if (index == _capacity) { 
+                index = 0;
+            }
+        }
+        std::cout << std::endl;
+    }
+
+private:
+    int _front;    
+    int _back;   
+    int* _array;  
+    int _capacity;
+    int _size;    
+
+   
+    void resize() {
+        int newCapacity = _capacity * 2;
+        int* newArray = new int[newCapacity];
+
+       
+        int index = _front;
+        for (int i = 0; i < _size; ++i) {
+            newArray[i] = _array[index];
+            index++;
+            if (index == _capacity) { 
+                index = 0;
+            }
+        }
+
+       
+        delete[] _array;
+        _array = newArray;
+        _capacity = newCapacity;
+        _front = 0;
+        _back = _size - 1;
+    }
+};
+
+int main() {
+    CircularQueue q(3); 
+
+    q.enqueue(10);
+    q.enqueue(20);
+    q.enqueue(30);
+    q.print(); 
+
+    q.enqueue(40); // This triggers a resize
+    q.print(); 
+
+    q.dequeue();
+    q.enqueue(50);
+    q.print(); 
+
+    return 0;
+}
